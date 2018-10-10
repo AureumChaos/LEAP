669a739ead58b50fb8e779431fefc5fdd27b4c4b
Coletti, Mark A <colettima@ornl.gov>
2018-10-10 14:58:20 -0400

    Start of a very basic GA example.

---
+++ Examples/ga.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+"""
+    A simple genetic algorithm example that uses the very basic One Max problem.
+
+"""
+import LEAP
+
+if __name__ == '__main__':
+
+    pop_size = 100
+    max_generation = 10
+
+    problem = problem.FunctionOptimization(problem.oneMaxFunction)
+
+
