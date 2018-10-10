0f50cef8430a6f76a4b976c0c52827fcefb7e959
Coletti, Mark A <colettima@ornl.gov>
2018-10-10 15:21:33 -0400

    Got something that *seems* to work, though the populations don't seem to change.

--- Examples/ga.py
+++ Examples/ga.py
@@ -3,13 +3,33 @@
     A simple genetic algorithm example that uses the very basic One Max problem.
 
 """
-import LEAP
+
+import problem  # TODO Need to fix this to be part of the LEAP space
+import decoder  # TODO ditto
+import ea
+import selection
+import operators
 
 if __name__ == '__main__':
 
-    pop_size = 100
+    pop_size = 20
     max_generation = 10
+    genome_size = 10
 
     problem = problem.FunctionOptimization(problem.oneMaxFunction)
+
+    decoder = decoder.BinaryDecoder(problem, genome_size)
+
+    ga = ea.GenerationalEA(decoder,
+                           operators.BitFlipMutation(
+                               operators.CloneOperator(
+                                   selection.ProportionalSelection()),
+                               1.0/genome_size),
+                               pop_size)
+
+    ga.run(maxGeneration=max_generation)
+
+
+
 
 
