c935358a219bc8318283625309e158605bc28e3b
Mark Coletti <colettima@ornl.gov>
2019-12-08 14:15:20 -0500

    Now test tournament selection support for pipeline args support.

--- test/leap/test_selection.py
+++ test/leap/test_selection.py
@@ -134,3 +134,24 @@ def test_tournament_selection():
     # This assert will sometimes not work because it's possible to select the same individual more than once, and that
     # includes scenarios where the worst of two individuals is selected twice, which can happen about 25% of the time.
     # assert pop[1] == best
+
+
+def test_tournament_with_args():
+    """ tournament selection support for pipeline argsv"""
+
+    pop = []
+
+    # Make a population where binary tournament has an obvious reproducible choice
+    pop.append(core.Individual([0, 0, 0], decoder=core.IdentityDecoder(), problem=binary_problems.MaxOnes()))
+    pop.append(core.Individual([1, 1, 1], decoder=core.IdentityDecoder(), problem=binary_problems.MaxOnes()))
+
+    i = iter(pop)
+
+    # We first need to evaluate all the individuals so that truncation selection has fitnesses to compare
+    pop = [individual for individual, args, kwargs in ops.evaluate(i)]
+
+    best, args, kwargs = next(ops.tournament(pop, 2, 44, foo='bar'))
+
+    assert args == (44,)
+    assert kwargs['foo'] == 'bar'
+
