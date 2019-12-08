2dcd3d34e96890f638c3974583decb3f12cd5c90
Mark Coletti <colettima@ornl.gov>
2019-12-08 12:47:35 -0500

    Added test for mu plus lambda, which passes

--- test/leap/test_selection.py
+++ test/leap/test_selection.py
@@ -56,9 +56,32 @@ def test_truncation_selection():
 
 
 def test_truncation_parents_selection():
-    # like above, but add possible parents
-    # TODO implement
-    pass
+    # Test (mu + lambda), i.e., parents competing with offspring
+    # Create parent and offspring populations such that each has an "best" individual that will be selected by
+    # truncation selection.
+
+    parents = []
+
+    parents.append(core.Individual([0, 0, 0], decoder=core.IdentityDecoder(), problem=binary_problems.MaxOnes()))
+    parents.append(core.Individual([1, 1, 0], decoder=core.IdentityDecoder(), problem=binary_problems.MaxOnes()))
+    i = iter(parents)
+    parents = [individual for individual, args, kwargs in ops.evaluate(i)]
+
+    offspring = []
+    offspring.append(core.Individual([0, 0, 1], decoder=core.IdentityDecoder(), problem=binary_problems.MaxOnes()))
+    offspring.append(core.Individual([1, 1, 1], decoder=core.IdentityDecoder(), problem=binary_problems.MaxOnes()))
+    i = iter(offspring)
+    offspring = [individual for individual, args, kwargs in ops.evaluate(i)]
+
+    parents_iter = iter(parents)
+    offspring_iter = iter(offspring)
+
+    truncated = ops.truncate(offspring_iter, 2, parents=parents_iter)
+
+    assert len(truncated) == 2
+
+    assert parents[1] in truncated
+    assert offspring[1] in truncated
 
 
 def test_truncation_selection_pipeline_args():
