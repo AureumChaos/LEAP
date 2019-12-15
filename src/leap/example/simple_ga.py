6370f5654cc4741439c8ea13bc2ebd084ff86b57
Mark Coletti <colettima@ornl.gov>
2019-12-15 17:15:19 -0500

    Crude primitive GA works.

--- src/leap/example/simple_ga.py
+++ src/leap/example/simple_ga.py
@@ -21,11 +21,14 @@ if __name__ == '__main__':
     max_generation = 5
 
     for generation in range(max_generation):
-        survivors, args, kwargs = pipe(parents,
+        survivors = pipe(parents,
                          ops.tournament,
                          ops.clone,
                          ops.mutate_bitflip,
+                         ops.evaluate,
                          ops.pool(size=10),  # 10 offspring
                          ops.truncate(size=5))  # (mu + lambda)
 
-        parents = list(offspring)
+        parents = survivors
+
+    pass
