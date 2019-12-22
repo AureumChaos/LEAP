5307d85b58d01899cb652cee737f257b0f891d6a
Mark Coletti <colettima@ornl.gov>
2019-12-21 22:17:16 -0500

    Enhanced simple GA example to use new inc_generation() function that uses context.

--- src/leap/example/simple_ga.py
+++ src/leap/example/simple_ga.py
@@ -9,7 +9,18 @@ from toolz import pipe
 from leap import core
 from leap import ops
 from leap import binary_problems
+from leap import util
 
+
+def print_population(population, generation):
+    """ Convenience function for pretty printing a population that's associated with a given generation
+
+    :param population:
+    :param generation:
+    :return: None
+    """
+    for individual in population:
+        print(generation, individual.genome, individual.fitness)
 
 if __name__ == '__main__':
     parents = core.Individual.create_population(5, initialize=core.create_binary_sequence,
@@ -18,11 +29,16 @@ if __name__ == '__main__':
     # Evaluate initial population
     parents = core.Individual.evaluate_population(parents)
 
+    # print initial, random population
+    print_population(parents, generation=0)
+
     max_generation = 5
 
-    # TODO add in context for tradcking and reporting generation as start of example of using context state
+    # We use the provided core.context, but we could roll our own if we wanted to keep
+    # separate contexts.  E.g., island models may want to have their own contexts.
+    generation_counter = util.inc_generation(context=core.context)
 
-    for generation in range(max_generation):
+    while generation_counter.generation() < max_generation:
         survivors = pipe(parents,
                          ops.tournament,
                          ops.clone,
@@ -32,5 +48,12 @@ if __name__ == '__main__':
                          ops.truncate(size=5))  # (mu + lambda)
 
         parents = survivors
+
+        generation_counter() # increment to the next generation
+
+        # Just to demonstrate that we can also get the current generation from the context
+        print_population(parents, core.context['leap']['generation'])
+
+
 
     pass
