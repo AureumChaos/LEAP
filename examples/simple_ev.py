e4d93bcfe95315c7b3d781e7a0663097116a1bac
Mark Coletti <colettima@ornl.gov>
2020-01-12 13:59:38 -0500

    EV now uses random parent selection.  Made some algorithm constants proper constants.

--- examples/simple_ev.py
+++ examples/simple_ev.py
@@ -25,7 +25,9 @@ def print_population(population, generation):
     for individual in population:
         print(generation, individual.genome, individual.fitness)
 
-BROOD_SIZE = 2 # how many offspring each parent will reproduce
+BROOD_SIZE = 3 # how many offspring each parent will reproduce
+POPULATION_SIZE = 10
+MAX_GENERATIONS = 5
 
 if __name__ == '__main__':
     # Define the real value bounds for initializing the population. In this case,
@@ -34,7 +36,7 @@ if __name__ == '__main__':
     # the (-5.12,5.12) was what was originally used for this problem in
     # Ken De Jong's 1975 dissertation, so was used for historical reasons.
     bounds = [(-5.12,5.12), (-5.12,5.12), (-5.12,5.12), (-5.12,5.12)]
-    parents = core.Individual.create_population(5,
+    parents = core.Individual.create_population(POPULATION_SIZE,
                                                 initialize=core.create_real_vector(bounds),
                                                 decoder=core.IdentityDecoder(),
                                                 problem=real_problems.SpheroidProblem(maximize=False))
@@ -45,7 +47,7 @@ if __name__ == '__main__':
     # print initial, random population
     print_population(parents, generation=0)
 
-    max_generation = 100
+    max_generation = MAX_GENERATIONS
 
     # We use the provided core.context, but we could roll our own if we wanted to keep
     # separate contexts.  E.g., island models may want to have their own contexts.
@@ -53,12 +55,12 @@ if __name__ == '__main__':
 
     while generation_counter.generation() < max_generation:
         offspring = pipe(parents,
-                         ops.cyclic_selection,
+                         ops.random_selection,
                          ops.clone,
                          ops.mutate_gaussian(std=.1),
                          ops.evaluate,
                          ops.pool(size=len(parents) * BROOD_SIZE), # create the brood
-                         ops.insertion_selection(parents=parents)) # mu + lambda
+                         ops.insertion_selection(parents=parents))
 
         parents = offspring
 
