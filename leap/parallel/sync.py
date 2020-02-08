d3b5f04988615c38479070037fcda4ddab288f17
Mark Coletti <colettima@ornl.gov>
2020-02-08 16:48:57 -0500

    Simplified function names to take advantage of module scope name.

--- leap/parallel/sync.py
+++ leap/parallel/sync.py
@@ -2,13 +2,59 @@
 """
   This provides a synchronous fitness evaluation pipeline operator.
 """
-impor math
+import math
 from toolz import curry
 
 from leap import core
 
+
+def evaluate(individual):
+    """ concurrently evaluate the given individual
+
+    This is what's invoked on each dask worker.
+
+    :param individual: to be evaluated
+    :return: evaluated individual
+    """
+    try:
+        individual.evaluate()
+    except Exception as e:
+        # Set fitness to NaN to indicate we *tried* to evaluate the
+        # individual; also save the associated exception so we can
+        # (hopefully) figure out what went wrong.
+        individual.fitness = math.nan
+        individual.exception = e
+
+        # We track the number of such failures on the off chance that this
+        # might be useful.
+        context['leap']['parallel']['non_viable'] += 1
+
+    return individual
+
+
+def eval_population(population, client, context=core.context):
+    """ Concurrently evaluate all the individuals in the given population
+
+    :param population: to be evaluated
+    :param client: dask client
+    :param context: for storing count of non-viable individuals
+    :return: evaluated population
+    """
+    # farm out population to worker nodes for evaluation
+    worker_futures = self.client.map(evaluate, population)
+
+    # now gather all the *completed* evaluations; note that some of the
+    # evaluations may complete much earlier than others, which means those
+    # related computational resources will idle until the last offspring is
+    # evaluated.  If this is a problem, please consider using async_eval_pool,
+    # instead.
+    evaluated_individuals = client.gather(worker_futures)
+
+    return evaluated_individuals
+
+
 @curry
-def sync_eval_pool(next_individual, client, size, context=core.context):
+def eval_pool(next_individual, client, size, context=core.context):
     """ concurrently evaluate `size` individuals
 
     This is similar to ops.pool() in that it's a "sink" for accumulating
@@ -31,40 +77,10 @@ def sync_eval_pool(next_individual, client, size, context=core.context):
     :param context: for storing count of non-viable individuals
     :return: the pool of evaluated individuals
     """
-    def evaluate(individual):
-        """ concurrently evaluate the given individual
-
-        This is what's invoked on each dask worker.
-
-        :param individual: to be evaluated
-        :return: evaluated individual
-        """
-        try:
-            individual.evaluate()
-        except Exception as e:
-            # Set fitness to NaN to indicate we *tried* to evaluate the
-            # individual; also save the associated exception so we can
-            # (hopefully) figure out what went wrong.
-            individual.fitness = math.nan
-            individual.exception = e
-
-            # We track the number of such failures on the off chance that this
-            # might be useful.
-            context['leap']['parallel']['non_viable'] += 1
-
-        return individual
-
     # First, accumulate individuals to be evaluated
     unevaluated_offspring = [next(next_individual) for _ in range(size)]
 
-    # farm out population to worker nodes for evaluation
-    worker_futures = self.client.map(evaluate, unevaluated_offspring)
-
-    # now gather all the *completed* evaluations; note that some of the
-    # evaluations may complete much earlier than others, which means those
-    # related computational resources will idle until the last offspring is
-    # evaluated.  If this is a problem, please consider using async_eval_pool,
-    # instead.
-    evaluated_offspring = client.gather(worker_futures)
+    evaluated_offspring = eval_population(unevaluated_offspring, client,
+                                          context)
 
     return evaluated_offspring
