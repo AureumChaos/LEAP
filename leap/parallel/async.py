a68401b372b85af16016aeb770152ad537773775
Mark Coletti <colettima@ornl.gov>
2020-02-13 11:25:25 -0500

    Added logging to leap.distributed.async. Added replace_if, insert_into_bag, and greedy_insert_into_bag.

--- leap/parallel/async.py
+++ leap/parallel/async.py
@@ -2,7 +2,14 @@
 """
   This provides an asynchronous steady-state fitness evaluation pipeline
   operator.
+
+  A common feature here is a "bag" of evaluated individuals that is
+  asynchronously updated via dask.  (We would use "pool instead of "bag",
+  but "pool" is already in use as ops.pool().)
+
 """
+import random
+import logging
 from toolz import curry
 
 from dask.distributed import as_completed
@@ -10,6 +17,19 @@ from dask.distributed import as_completed
 from leap import core
 
 from evaluate import evaluate
+
+# Create unique logger for this namespace
+logger = logging.getLogger(__name__)
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+console_handler.setFormatter(formatter)
+
+logger.addHandler(console_handler)
 
 
 def eval_population(population, client, context=core.context):
@@ -28,31 +48,49 @@ def eval_population(population, client, context=core.context):
     return as_completed(worker_futures)
 
 
-@curry
-def eval_pool(next_individual, client, futures_iter, pool_size, num_births,
-              context=core.context):
-    """ Asynchronously evaluate `size` individuals
+def replace_if(new_individual, bag, index):
+    """ Convenience function for possibly replacing bag[index] individual
+    with new_individual depending on which has higher fitness.
 
-    This is similar to ops.pool() in that it's a "sink" for accumulating
-    individuals by "pulling" individuals from upstream the pipeline via
-    `next_individual`.  However, it's also like ops.evaluate() in that
-    these individuals are concurrently evaluated via a map/reduce approach. We
-    use dask to implement this evaluation mechanism.
-
-    If an exception is thrown while evaluating an individual, NaN is assigned as
-    its fitness, individual.is_viable is set to False, and the associated
-    exception is assigned to individual.exception as a post mortem aid; also
-    core.context['leap']['parallel']['non_viables'] count is incremented if you
-    want to track the number of non-viable individuals (i.e., those that have
-    an exception thrown during evaluation); just remember to reset that between
-    runs if that variable has been updated.
-
-    :param next_individual: iterator/generator for individual provider
-    :param client: dask client through which we submit individuals to be evaluated
-    :param futures_iter: dask distributed iterator to futures of individuals
-        that are being evaluated or have finished evaluating
-    :param pool_size: how many evaluated individuals to keep
-    :param num_births: birth budget
-    :param context: for storing count of non-viable individuals
-    :return: the pool of evaluated individuals
+    :param new_individual: is a newly evaluated individual
+    :param second:
+    :param bag: of already evaluated individuals
+    :param index: of individual in bag to be compared against
+    :return: None
     """
+    if new_individual > bag[index]:
+        logger.debug('Replaced %s with %s', new_individual, bag[index])
+        bag[index] = new_individual
+    else:
+        logger.debug('%s < %s, so the latter stays in bag', new_individual,
+                     bag[index])
+
+
+def insert_into_bag(indivdidual, bag):
+    """ Insert the given individual into the bag of evaluated individuals.
+
+    Randomly select an individual in the bag, and the `individual` will
+    replace the selected individual iff it has a better fitness.
+
+    :param indivdidual: that was just evaluated
+    :param bag: of already evaluated individuals
+    :return: None
+    """
+    rand_index = random.randrange(len(bag))
+    replace_if(indivdidual, rand_index, bag)
+
+
+
+def greedy_insert_into_bag(individual, bag):
+    """ Insert the given individual into the bag of evaluated individuals.
+
+    This is greedy because we always compare the new `individual` with the
+    current weakest in the bag.
+
+    :param individual: that was just evaluated
+    :param bag: of already evaluated individuals
+    :return: None
+    """
+    # From https://stackoverflow.com/questions/2474015/getting-the-index-of-the-returned-max-or-min-item-using-max-min-on-a-list
+    index_min = min(range(len(bag)), key=bag.__getitem__)
+    replace_if(individual, bag, index_min)
