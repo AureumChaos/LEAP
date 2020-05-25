71517ae4ab54e1f73d107cf0f93bb3c63b37207b
Mark Coletti <colettima@ornl.gov>
2020-05-25 17:27:55 -0400

    PEP8 compliance for brains.py

--- leap/brains.py
+++ leap/brains.py
@@ -1,7 +1,9 @@
-"""This module provides machinery for representing and evolving executable objects (i.e. \"brains\") of various kinds.
+""" This module provides machinery for representing and evolving executable
+objects (i.e. \"brains\") of various kinds.
 
-A LEAP \"brain\" is a kind of phenotype, and it is constructed when we use a :class:`~leap.core.Decoder` to convert a
-genotypic representation of the brain into an executable phenotype.
+A LEAP \"brain\" is a kind of phenotype, and it is constructed when we use a
+:class:`~leap.core.Decoder` to convert a genotypic representation of the
+brain into an executable phenotype.
 
 """
 
@@ -31,12 +33,14 @@ class RandomBrain(Brain):
     A trivial 'brain' that samples a random value from its output space.
 
     :param input_space: space of possible inputs (ignored)
-    :param output_space: the space of possible actions to sample from, satisfying the `Space` interface used by
-        OpenAI Gym
+
+    :param output_space: the space of possible actions to sample from,
+    satisfying the `Space` interface used by OpenAI Gym
     """
+
     def __init__(self, input_space, output_space):
-        assert(output_space is not None)
-        assert(hasattr(output_space, 'sample'))
+        assert (output_space is not None)
+        assert (hasattr(output_space, 'sample'))
         self.input_space = input_space
         self.output_space = output_space
 
@@ -67,31 +71,40 @@ class RandomBrain(Brain):
 ##############################
 class KeyboardBrain(Brain):
     """
-    A non-autonomous 'brain' that allows users to control an agent via the keyboard.
+    A non-autonomous 'brain' that allows users to control an agent via the
+    keyboard.
 
     :param input_space: space of possible inputs (ignored)
-    :param output_space: the space of possible actions to sample from, satisfying the `Space` interface used by
-        OpenAI Gym
+
+    :param output_space: the space of possible actions to sample from,
+    satisfying the `Space` interface used by OpenAI Gym
+
     :param keymap: `dict` mapping keys to elements of the output space
     """
 
     def __init__(self, input_space, output_space,
-                 keymap=lambda x: int(x - ord('0')) if x in range(ord('0'), ord('9')) else 0):
-        assert(output_space is not None)
+                 keymap=lambda x: int(x - ord('0')) if x in range(ord('0'), ord(
+                     '9')) else 0):
+        assert (output_space is not None)
         if np.prod(output_space.shape) > 1:
-            raise ValueError("This environment requires a 'brain' with {0} ".format(np.prod(output_space.shape)) +
-                             "outputs, but {0} can only produce 1 output at a time.".format(KeyboardBrain.__name__))
-        assert(keymap is not None)
+            raise ValueError(
+                "This environment requires a 'brain' with {0} ".format(
+                    np.prod(output_space.shape)) +
+                "outputs, but {0} can only produce 1 output at a time.".format(
+                    KeyboardBrain.__name__))
+        assert (keymap is not None)
         self.output_space = output_space
         self.keymap = keymap
         self.action = 0
 
     def key_press(self, key, mod):
-        """You'll need to assign this function to your environment's key_press handler."""
+        """You'll need to assign this function to your environment's
+        key_press handler. """
         self.action = self.keymap(key)
 
     def key_release(self, key, mod):
-        """You'll need to assign this function to your environment's key_release handler."""
+        """You'll need to assign this function to your environment's
+        key_release handler. """
         if self.keymap(key) == self.action:
             self.action = 0
 
@@ -104,7 +117,8 @@ class KeyboardBrain(Brain):
 # Class BrainProblem
 ##############################
 class BrainProblem(real_problems.ScalarProblem):
-    def __init__(self, runs, steps, environment, behavior_fitness, stop_on_done=True, maximize=True):
+    def __init__(self, runs, steps, environment, behavior_fitness,
+                 stop_on_done=True, maximize=True):
         super().__init__(maximize)
         self.runs = runs
         self.steps = steps
@@ -152,27 +166,35 @@ def survival_fitness(observations, rewards):
 ##############################
 class PittRulesBrain(Brain):
     """
-    A 'brain' that interprets a Pittsburgh-style ruleset and outputs the appropriate action.
+    A 'brain' that interprets a Pittsburgh-style ruleset and outputs the
+    appropriate action.
 
     :param input_space: an OpenAI-gym-style space defining the inputs
     :param output_space: an OpenAI-gym-style space defining the outputs
     :param init_memory: a list of initial values for the memory registers
-    :param rules: a list of rules, each of the form `[ c1 c1'  c2 c2' ... cn cn'  a1 ... am m1 ... mr]`
-    :param priority_metric: the rule prioritization strategy used to resolve conflicts
 
-    Rulesets are lists of rules.  Rules are lists of the form `[ c1 c1'  c2 c2' ... cn cn'  a1 ... am m1 ... mr]`, where
-    `(cx, cx')` are are the min and max bounds that the rule covers, `a1 .. am` are the output actions, and `m1 ... mr`
-    are values to write to the memory registers.
+    :param rules: a list of rules, each of the form `[ c1 c1'  c2 c2' ... cn
+    cn'  a1 ... am m1 ... mr]`
 
-    For example, this ruleset has two rules.  The first rule covers the square bounded by `(0.0, 0.6)' and `(0.0, 0.4)`,
-    returning the output action `0` if the input falls within that range:
+    :param priority_metric: the rule prioritization strategy used to resolve
+    conflicts
+
+    Rulesets are lists of rules.  Rules are lists of the form `[ c1 c1'  c2
+    c2' ... cn cn'  a1 ... am m1 ... mr]`, where `(cx, cx')` are are the min
+    and max bounds that the rule covers, `a1 .. am` are the output actions,
+    and `m1 ... mr` are values to write to the memory registers.
+
+    For example, this ruleset has two rules.  The first rule covers the
+    square bounded by `(0.0, 0.6)' and `(0.0, 0.4)`, returning the output
+    action `0` if the input falls within that range:
 
     >>> rules = [[0.0,0.6, 0.0,0.4, 0],
     ...          [0.4,1.0, 0.6,1.0, 1]]
 
-    The input and output spaces are defined in the style of OpenAI gym.  For example, here's how you would set up a
-    PittRulesBrain with the above ruleset that takes two continuous input variables on `(0.0, 1.0)`, and outputs
-    discrete values in `{0, 1}`:
+    The input and output spaces are defined in the style of OpenAI gym.  For
+    example, here's how you would set up a PittRulesBrain with the above
+    ruleset that takes two continuous input variables on `(0.0, 1.0)`,
+    and outputs discrete values in `{0, 1}`:
 
     >>> import numpy as np
     >>> from gym import spaces
@@ -184,12 +206,14 @@ class PittRulesBrain(Brain):
 
     PriorityMetric = Enum('PriorityMetric', 'RULE_ORDER GENERALITY PERIMETER')
 
-    def __init__(self, input_space, output_space, rules, priority_metric, init_mem=[]):
-        assert(input_space is not None)
-        assert(output_space is not None)
-        assert(rules is not None)
-        assert(len(rules) > 0)
-        assert(priority_metric in PittRulesBrain.PriorityMetric.__members__.values())
+    def __init__(self, input_space, output_space, rules, priority_metric,
+                 init_mem=[]):
+        assert (input_space is not None)
+        assert (output_space is not None)
+        assert (rules is not None)
+        assert (len(rules) > 0)
+        assert (
+                    priority_metric in PittRulesBrain.PriorityMetric.__members__.values())
 
         self.input_space = input_space
         self.num_inputs = int(np.prod(input_space.shape))
@@ -198,7 +222,8 @@ class PittRulesBrain(Brain):
         self.memory_registers = np.array(init_mem)
         self.num_memory = len(self.memory_registers)
         self.rules = rules
-        self.priorities = [self.__priority(r, rule, priority_metric) for (r, rule) in enumerate(rules)]
+        self.priorities = [self.__priority(r, rule, priority_metric) for
+                           (r, rule) in enumerate(rules)]
 
     def __priority(self, rule_order, rule, priority_metric):
         """Compute the priority value to a given rule."""
@@ -209,7 +234,8 @@ class PittRulesBrain(Brain):
         elif priority_metric == PittRulesBrain.PriorityMetric.PERIMETER:
             pass
         else:
-            raise ValueError('Unrecognized priority_metric "{0}".'.format(priority_metric))
+            raise ValueError(
+                'Unrecognized priority_metric "{0}".'.format(priority_metric))
 
     def __match_set(self, input):
         """Build the match set for a set of rules."""
@@ -221,7 +247,8 @@ class PittRulesBrain(Brain):
         for (r, rule) in enumerate(self.rules):
             match_score = 0
             for c in range(len(all_input)):  # Loop through all conditions
-                diff1 = rule[c * 2] - all_input[c]  # TODO Normalize this, in case the possible ranges differ greatly
+                diff1 = rule[c * 2] - all_input[
+                    c]  # TODO Normalize this, in case the possible ranges differ greatly
                 diff2 = rule[c * 2 + 1] - all_input[c]
                 if diff1 * diff2 <= 0:  # Check sign
                     diff = 0  # Within the range
@@ -303,16 +330,22 @@ class PittRulesBrain(Brain):
         # If our best-matching rules are exact matches
         if best_match_score == 0:
             # then cull the matchList based on priority.
-            highest_priority_rule = min(match_list, key=lambda x: self.priorities[x])
+            highest_priority_rule = min(match_list,
+                                        key=lambda x: self.priorities[x])
             highest_priority_value = self.priorities[highest_priority_rule]
-            match_list = [r for r in match_list if self.priorities[r] <= highest_priority_value]
+            match_list = [r for r in match_list if
+                          self.priorities[r] <= highest_priority_value]
 
-        # Among the rules set of rules with the highest priority, choose one randomly.
-        # TODO Implement other common conflict-resolutions strategies: like picking the output with the most rules
+        # Among the rules set of rules with the highest priority, choose one
+        # randomly.
+
+        # TODO Implement other common conflict-resolutions strategies: like
+        #  picking the output with the most rules
+
         # TODO advocating for it (i.e. vote).
         winner = np.random.choice(match_list)
         output = np.round(self.__fire(winner)).astype(int).tolist()
-        assert(len(output) == self.num_outputs)
+        assert (len(output) == self.num_outputs)
         if self.num_outputs > 1:
             return output  # Return a list of outputs if there are more than one
         else:
@@ -323,10 +356,11 @@ class PittRulesBrain(Brain):
 # Class PittRulesDecoder
 ##############################
 class PittRulesDecoder(core.Decoder):
-    def __init__(self, input_space, output_space, priority_metric, num_memory_registers):
-        assert(input_space is not None)
-        assert(output_space is not None)
-        assert(num_memory_registers >= 0)
+    def __init__(self, input_space, output_space, priority_metric,
+                 num_memory_registers):
+        assert (input_space is not None)
+        assert (output_space is not None)
+        assert (num_memory_registers >= 0)
         self.input_space = input_space
         self.num_inputs = int(np.prod(input_space.shape))
         self.output_space = output_space
@@ -335,10 +369,10 @@ class PittRulesDecoder(core.Decoder):
         self.num_memory_registers = num_memory_registers
 
     def decode(self, genome):
-        assert(genome is not None)
-        assert(len(genome) > 0)
-        rule_length = self.num_inputs*2 + self.num_outputs + self.num_memory_registers
-        assert(len(genome) % rule_length == 0)
+        assert (genome is not None)
+        assert (len(genome) > 0)
+        rule_length = self.num_inputs * 2 + self.num_outputs + self.num_memory_registers
+        assert (len(genome) % rule_length == 0)
         rules = np.reshape(genome, (-1, rule_length))
-        return PittRulesBrain(self.input_space, self.output_space, rules, self.priority_metric)
-
+        return PittRulesBrain(self.input_space, self.output_space, rules,
+                              self.priority_metric)
