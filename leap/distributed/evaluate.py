75f7e13ae4c2614c356db51de06d6ddc18f45384
Mark Coletti <colettima@ornl.gov>
2020-06-14 00:52:13 -0400

    Move exception handling logic from distributed.evaluate to core.Individual.evaluate.

--- leap/distributed/evaluate.py
+++ leap/distributed/evaluate.py
@@ -40,24 +40,19 @@ def evaluate(individual, context=core.context):
     :param individual: to be evaluated
     :return: evaluated individual
     """
-    try:
-        worker = get_worker()
+    worker = get_worker()
 
-        individual.start_eval_time = time.time()
+    individual.start_eval_time = time.time()
 
-        if hasattr(worker, 'logger'):
-            worker.logger.debug(
-                f'Worker {worker.id} started evaluating {individual!s}')
-        individual.evaluate()
-        individual.is_viable = True
-    except Exception as e:
-        # Set fitness to NaN to indicate we *tried* to evaluate the
-        # individual; also save the associated exception so we can
-        # (hopefully) figure out what went wrong.
-        individual.fitness = math.nan
-        individual.is_viable = False  # TODO maybe the NaN is sufficient?
-        individual.exception = e
+    if hasattr(worker, 'logger'):
+        worker.logger.debug(
+            f'Worker {worker.id} started evaluating {individual!s}')
 
+    # Any thrown exceptions are now handled inside Individual.evaluate()
+    individual.evaluate()
+
+    if not individual.is_viable:
+        # is_viable will be False if an exception was thrown during evaluation.
         # We track the number of such failures on the off chance that this
         # might be useful.
         context['leap']['distributed']['non_viable'] += 1
@@ -65,15 +60,16 @@ def evaluate(individual, context=core.context):
         if hasattr(worker, 'logger'):
             worker.logger.warning(
                 f'Worker {worker.id}: {e} raised for {individual!s}')
-    finally:
-        individual.stop_eval_time = time.time()
-        individual.hostname = platform.node()
-        individual.pid = os.getpid()
-        if hasattr(worker, 'logger'):
-            worker.logger.debug(
-                f'Worker {worker.id} evaluated {individual!s} in '
-                f'{individual.stop_eval_time - individual.start_eval_time} '
-                f'seconds')
+
+    individual.stop_eval_time = time.time()
+    individual.hostname = platform.node()
+    individual.pid = os.getpid()
+
+    if hasattr(worker, 'logger'):
+        worker.logger.debug(
+            f'Worker {worker.id} evaluated {individual!s} in '
+            f'{individual.stop_eval_time - individual.start_eval_time} '
+            f'seconds')
 
     return individual
 
