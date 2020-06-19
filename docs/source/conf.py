bb51271d59fadda6867bbcbd666f28ee6ed4ea9a
Mark Coletti <colettima@ornl.gov>
2020-06-19 13:55:54 -0400

    Added a version and the roadmap to the documentation.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,9 +24,9 @@ copyright = '2019, Jeffrey K. Bassett, Mark Coletti, and Eric O. Scott'
 author = 'Jeffrey K. Bassett, Mark Coletti, and Eric O. Scott'
 
 # The short X.Y version
-version = ''
+version = '0.2'
 # The full version, including alpha/beta/rc tags
-release = ''
+release = 'version 0.2.0'
 
 
 # -- General configuration ---------------------------------------------------
