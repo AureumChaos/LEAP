b2324a094e45b5a4282ccaf6fbafe9f49cc6ea6f
Mark Coletti <colettima@ornl.gov>
2020-09-11 15:19:40 -0400

    Created binary initializers.py

---
+++ leap_ec/binary_rep/initializers.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+    Used to initialize binary sequences
+"""
+
+##############################
+# Closure create_binary_sequence
+##############################
+def create_binary_sequence(length):
+    """
+    A closure for initializing a binary sequences for binary genomes.
+
+    :param length: how many genes?
+
+    :return: a function that, when called, generates a binary vector of given
+        length
+
+    E.g., can be used for `Individual.create_population`
+
+    >>> from leap_ec import core, binary_problems
+    >>> population = Individual.create_population(10, core.create_binary_sequence(length=10),
+    ...                                           decoder=core.IdentityDecoder(),
+    ...                                           problem=binary_problems.MaxOnes())
+
+    """
+
+    def create():
+        return [random.choice([0, 1]) for _ in range(length)]
+
+    return create
--- leap_ec/core.py
+++ leap_ec/core.py
@@ -32,32 +32,6 @@ from leap_ec import util
 context = {'leap': {'distributed': {'non_viable': 0}}}
 
 
-##############################
-# Closure create_binary_sequence
-##############################
-def create_binary_sequence(length):
-    """
-    A closure for initializing a binary sequences for binary genomes.
-
-    :param length: how many genes?
-
-    :return: a function that, when called, generates a binary vector of given
-        length
-
-    E.g., can be used for `Individual.create_population`
-
-    >>> from leap_ec import core, binary_problems
-    >>> population = Individual.create_population(10, core.create_binary_sequence(length=10),
-    ...                                           decoder=core.IdentityDecoder(),
-    ...                                           problem=binary_problems.MaxOnes())
-
-    """
-
-    def create():
-        return [random.choice([0, 1]) for _ in range(length)]
-
-    return create
-
 
 ##############################
 # Closure create_real_vector
