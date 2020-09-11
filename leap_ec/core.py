61a165543153332b8e17ffed4f23ae2b25bb9622
Mark Coletti <colettima@ornl.gov>
2020-09-11 15:59:43 -0400

    Moved context and representation to their own modules

--- leap_ec/core.py
+++ leap_ec/core.py
@@ -19,48 +19,6 @@ from toolz.itertoolz import pluck
 
 from leap_ec import util
 
-# This defines a global context that is a dictionary of dictionaries.  The
-# intent is for certain operators and functions to add to and modify this
-# context.  Third party operators and functions will just add a new top-level
-# dedicated key.
-# context['leap'] is for storing general LEAP running state, such as current
-#    generation.
-# context['leap']['distributed'] is for storing leap.distributed running state
-# context['leap']['distributed']['non_viable'] accumulates counts of non-viable
-#    individuals during distributed.eval_pool() and
-#    distributed.async_eval_pool() runs.
-context = {'leap': {'distributed': {'non_viable': 0}}}
 
-
-
-
-
-##############################
-# Class Representation
-##############################
-class Representation():
-    """A `Representation` is a simple data structure that wraps the
-    components needed to define, initialize, and decode individuals.
-
-    This just serves as some syntactic sugar when we are specifying
-    algorithms---so that representation-related components are grouped
-    together and clearly labeled `Representation`. """
-
-    def __init__(self, decoder, initialize, individual_cls=Individual):
-        self.decoder = decoder
-        self.initialize = initialize
-        self.individual_cls = individual_cls
-
-    def create_population(self, pop_size, problem):
-        """ make a new population
-
-        :param pop_size: how many individuals should be in the population
-        :param problem: to be solved
-        :return: a population of `individual_cls` individuals
-        """
-        return self.individual_cls.create_population(pop_size,
-                                                     initialize=self.initialize,
-                                                     decoder=self.decoder,
-                                                     problem=problem)
 
 
---
+++ leap_ec/representation.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+A `Representation` is a simple data structure that wraps the
+components needed to define, initialize, and decode individuals.
+
+This just serves as some syntactic sugar when we are specifying
+algorithms---so that representation-related components are grouped
+together and clearly labeled `Representation`.
+"""
+from .. individual import Individual
+
+##############################
+# Class Representation
+##############################
+class Representation():
+    """ Syntactic sugar for some of the monolithic functions that
+        conveniently combines a decoder, initializer, and an Individual
+        class since those always work in tandem, but can still be loosely
+        coupled.
+     """
+
+    def __init__(self, decoder, initialize, individual_cls=Individual):
+        self.decoder = decoder
+        self.initialize = initialize
+        self.individual_cls = individual_cls
+
+    def create_population(self, pop_size, problem):
+        """ make a new population
+
+        :param pop_size: how many individuals should be in the population
+        :param problem: to be solved
+        :return: a population of `individual_cls` individuals
+        """
+        return self.individual_cls.create_population(pop_size,
+                                                     initialize=self.initialize,
+                                                     decoder=self.decoder,
+                                                     problem=problem)
