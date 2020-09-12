113978a77c067bd7e820cbafd6777311a2a199e6
Mark Coletti <colettima@ornl.gov>
2020-09-11 23:04:55 -0400

    All top level unit tests now work with reorganized code.

--- tests/test_clone.py
+++ tests/test_clone.py
@@ -1,18 +1,19 @@
 """
     Unit tests for cloning
 """
-from leap_ec import core
-from leap_ec import binary_problems
-from leap_ec import ops
+from leap_ec.individual import Individual
+from leap_ec.decoder import IdentityDecoder
+from leap_ec.binary_rep.problems import MaxOnes
+import leap_ec.ops as ops
 
 
 def test_clone():
     # We need an encoder and problem to ensure those float across during
     # clones.
-    decoder = core.IdentityDecoder()
-    problem = binary_problems.MaxOnes()
+    decoder = IdentityDecoder()
+    problem = MaxOnes()
 
-    original = core.Individual([1, 1], decoder=decoder, problem=problem)
+    original = Individual([1, 1], decoder=decoder, problem=problem)
 
     cloned = next(ops.clone(iter([original])))
 
