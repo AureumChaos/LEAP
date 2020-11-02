91e2c2066a97eb4e10a1714749091de46def4749
Mark Coletti <colettima@ornl.gov>
2020-11-02 16:24:14 -0500

    Added doctest for SegmentedDecoder

--- leap_ec/segmented_rep/decoders.py
+++ leap_ec/segmented_rep/decoders.py
@@ -10,10 +10,19 @@ from leap_ec.decoder import Decoder
 class SegmentedDecoder(Decoder):
     """
         For decoding LEAP segmented representations
+
+        >>> from leap_ec.binary_rep.decoders import BinaryToIntDecoder
+
+        This example presumes that each segment has five bits, the first to
+        map to an integer and the remaining three to a different integer.
+
+        >>> decoder = SegmentedDecoder(BinaryToIntDecoder(2,3))
+        >>> genome = [[1, 0, 1, 0, 1], [0, 0, 1, 1, 1], [1, 0, 0, 0, 1]]
+        >>> vals = decoder.decode(genome)
+        >>> assert vals == [[2, 5], [0, 7], [2, 1]]
     """
     def __init__(self, segment_decoder):
         """
-
         :param segment_decoder: is the decoder used for all segments
         :type segment_decoder:
         """
@@ -26,14 +35,14 @@ class SegmentedDecoder(Decoder):
             For decoding `genome` which is a list of lists, or a
             segmented representation.
 
-        :param genome:
-        :type genome:
-        :param args:
-        :type args:
-        :param kwargs:
-        :type kwargs:
-        :return:
-        :rtype:
+        :param genome: for a given individual
+        :type genome: will be a list of segments (or lists)
+        :param args: optional args
+        :type args: list
+        :param kwargs: optional keyword args
+        :type kwargs: dict
+        :return: a list of list of values decoded from `genome`
+        :rtype: list
         """
         values = [self.segment_decoder.decode(segment) for segment in genome]
 
