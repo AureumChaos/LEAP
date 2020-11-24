acfff972c0ca6c882175c5d6684ee30bb64e5d0b
Mark Coletti <colettima@ornl.gov>
2020-11-24 16:28:45 -0500

    Fixed signature for individual_mutate_randint, and also greatly simplified its implementation.

--- leap_ec/int_rep/ops.py
+++ leap_ec/int_rep/ops.py
@@ -14,10 +14,10 @@ from leap_ec.ops import compute_expected_probability, iteriter_op
 def mutate_randint(next_individual: Iterator, bounds,
                    expected_num_mutations: float = 1) -> Iterator:
     """Perform randint mutation on each individual in an iterator (population).
-    
+
     This operator replaces randomly selected genes with an integer samples
     from a uniform distribution.
-    
+
     >>> from leap_ec.individual import Individual
     >>> from leap_ec.int_rep.ops import mutate_randint
 
@@ -43,18 +43,29 @@ def mutate_randint(next_individual: Iterator, bounds,
 # Function individual_mutate_randint
 ##############################
 @curry
-def individual_mutate_randint(genome: Iterator, bounds,
-                              expected_num_mutations: float = 1) -> Iterator:
-    """Perform random-integer mutation on a particular genome."""
-    probability = compute_expected_probability(expected_num_mutations,
-                                               genome)
+def individual_mutate_randint(genome: list,
+                              bounds: list,
+                              expected_num_mutations: float = 1) -> list:
+    """ Perform random-integer mutation on a particular genome.
 
-    def randomint_mutate(gene_index, value, probability):
-        """randomint-mutate a bit given a probability
+        >>> genome = [42, 12]
+        >>> bounds = [(0,50), (-10,20)]
+        >>> new_genome = individual_mutate_randint(genome, bounds)
+
+        :param genome: sequence of integers to be mutated
+        :param bounds: sequence of bounds tuples; e.g., [(1,2),(3,4)]
+        :param expected_num_mutations: on average how many mutations done
+    """
+    def randomint_mutate(value, bound, probability):
+        """ mutate an integer given a probability
         """
         if random.random() < probability:
-            return random.randint(*bounds[gene_index])
+            return random.randint(*bound)
         else:
             return value
 
-    return [randomint_mutate(i, gene, probability) for i, gene in enumerate(genome)]
+    probability = compute_expected_probability(expected_num_mutations, genome)
+
+    genome = [randomint_mutate(gene, bound, probability) for gene, bound in zip(genome,bounds)]
+
+    return genome
