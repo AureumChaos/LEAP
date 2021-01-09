a9479743969f9e21e59e0d529d57234ef79c6335
Mark Coletti <colettima@ornl.gov>
2021-01-09 17:29:35 -0500

    Version 0.5 changes.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name='leap_ec',
-    version='0.4.1',
+    version='0.5.0',
     packages=find_packages(),
     license='Academic',
     author='Mark Coletti, Eric Scott, Jeff Bassett',
