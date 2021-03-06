03d46b0620783290b23fb4b7065e68e36a8710e8
SigmaX <ericsiggyscott@gmail.com>
2021-03-06 09:44:43 -0500

    Add 'if __name__ == "__main__"' to examples/simple.py, so it doesn't run the whole script every time tests are collected.

--- examples/simple.py
+++ examples/simple.py
@@ -9,8 +9,7 @@ from leap_ec.simple import ea_solve
 def function(values):
     return sum([x ** 2 for x in values])
 
-# FIXME this returns NaNs, so a perfect opportunity for testing out the
-# new RobustIndividual
-ea_solve(function,
-         bounds=[(-5.12, 5.12) for _ in range(5)],
-         mutation_std=0.1)
+if __name__ == '__main__':
+    ea_solve(function,
+            bounds=[(-5.12, 5.12) for _ in range(5)],
+            mutation_std=0.1)
