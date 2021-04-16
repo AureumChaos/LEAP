e2e7ffab0498743d214e5426882bd4b6a79b0c29
Mark Coletti <colettima@ornl.gov>
2021-04-16 17:17:13 -0400

    Accidentally clobbered install packages in setup.  But trying dask[complete] to see if that works.

--- setup.py
+++ setup.py
@@ -21,8 +21,8 @@ setup(
     ],
     python_requires='>=3.6',
     install_requires=[
-        'dask',         # Used for parallel and distrib algorithms
-        'distrib',  # Used for parallel and distrib algorithms
+        'dask[complete]', # Used for parallel and distributed algorithms
+        #'distributed',  # Used for parallel and distributed algorithms
         'matplotlib',   # Used in visualizations
         'networkx',     # Used to specify island model topologies
         'numpy',        # Used for vector math
