523376b1e8dc0c225f03d7182679485a5f734ec6
Mark Coletti <colettima@ornl.gov>
2021-04-30 14:02:48 -0400

    Added sanity check to ensure that NaN handling worked for selection for minimization

--- tests/test_selection.py
+++ tests/test_selection.py
@@ -2,10 +2,12 @@
     Unit test for selection operators.
 """
 import random
+from math import nan
 
 from leap_ec.individual import Individual
 from leap_ec.decoder import IdentityDecoder
 from leap_ec.binary_rep.problems import MaxOnes
+from leap_ec.real_rep.problems import SpheroidProblem
 import leap_ec.ops as ops
 
 
@@ -34,16 +36,19 @@ def test_naive_cyclic_selection():
 def test_cyclic_selection():
     """ Test of the deterministic cyclic selection """
 
-    # Set seed so that we get consistent test results.  I.e., it is possible by
-    # happenstance for some tests to fail even though they're actually ok.  E.g.,
-    # the cyclic selection tests will test if the test_sequence shuffles between a
-    # complete cycle, but there's a chance that the same test_sequence may come up in
-    # the random shuffle, so the test will fail.  However, if we set a random seed
-    # ahead of time, then we can control for those pathological scenarios.
+    # Set seed so that we get consistent test results.  I.e., it is possible
+    # by happenstance for some tests to fail even though they're actually ok.
+    # E.g., the cyclic selection tests will test if the test_sequence
+    # shuffles between a complete cycle, but there's a chance that the same
+    # test_sequence may come up in the random shuffle, so the test will fail.
+    # However, if we set a random seed ahead of time, then we can control for
+    # those pathological scenarios.
     random.seed(123)
 
-    # We're just going to use integers for the population as that's sufficient for testing this selection
-    # operator; we don't want to get in the weeds with comparing individuals for test_sequence equivalency testing.
+    # We're just going to use integers for the population as that's
+    # sufficient for testing this selection operator; we don't want to get in
+    # the weeds with comparing individuals for test_sequence equivalency
+    # testing.
     pop = list(range(4))
 
     # This selection operator will deterministically cycle through the
@@ -69,14 +74,16 @@ def test_truncation_selection():
            Individual([1, 1, 0], decoder=IdentityDecoder(), problem=MaxOnes()),
            Individual([1, 1, 1], decoder=IdentityDecoder(), problem=MaxOnes())]
 
-    # We first need to evaluate all the individuals so that truncation selection has fitnesses to compare
+    # We first need to evaluate all the individuals so that truncation
+    # selection has fitnesses to compare
     pop = Individual.evaluate_population(pop)
 
     truncated = ops.truncation_selection(pop, 2)
 
     assert len(truncated) == 2
 
-    # Just to make sure, check that the two best individuals from the original population are in the selected population
+    # Just to make sure, check that the two best individuals from the
+    # original population are in the selected population
     assert pop[2] in truncated
     assert pop[3] in truncated
 
@@ -106,20 +113,63 @@ def test_truncation_parents_selection():
 
 def test_tournament_selection():
     """ This simple binary tournament_selection selection """
-    # Make a population where binary tournament_selection has an obvious reproducible choice
+    # Make a population where binary tournament_selection has an obvious
+    # reproducible choice
     pop = [Individual([0, 0, 0], decoder=IdentityDecoder(), problem=MaxOnes()),
            Individual([1, 1, 1], decoder=IdentityDecoder(), problem=MaxOnes())]
 
-    # We first need to evaluate all the individuals so that truncation selection has fitnesses to compare
+    # We first need to evaluate all the individuals so that truncation
+    # selection has fitnesses to compare
     pop = Individual.evaluate_population(pop)
 
     best = next(ops.tournament_selection(pop))
     pass
 
-    # This assert will sometimes not work because it's possible to select the same individual more than once, and that
-    # includes scenarios where the worst of two individuals is selected twice, which can happen about 25% of the time.
-    # assert pop[1] == best
+    # This assert will sometimes not work because it's possible to select the
+    # same individual more than once, and that includes scenarios where the
+    # worst of two individuals is selected twice, which can happen about 25%
+    # of the time. assert pop[1] == best
 
 
 
+def test_tournament_selection_with_nan():
+    """ This simple binary tournament_selection selection to test NaN behavior
+    """
+    # Make a population where binary tournament_selection has an obvious
+    # reproducible choice
+    problem = MaxOnes()
+    pop = [Individual([0, 0, 0], decoder=IdentityDecoder(), problem=problem),
+           Individual([1, 1, 1], decoder=IdentityDecoder(), problem=problem)]
 
+    # We first need to evaluate all the individuals so that truncation
+    # selection has fitnesses to compare
+    pop = Individual.evaluate_population(pop)
+
+    # Now set the "best" to NaN
+    pop[1].fitness = nan
+
+    best = ops.truncation_selection(pop, size=1)
+
+    assert pop[0] == best[0]
+
+    # Ok, now do this for _minimization_
+    decoder = IdentityDecoder()
+    problem = SpheroidProblem(maximize=False)
+
+    pop = []
+
+    pop.append(Individual([0], problem=problem, decoder=decoder))
+    pop.append(Individual([1], problem=problem, decoder=decoder))
+
+    pop = Individual.evaluate_population(pop)
+
+    # First *normal* selection should yield the 0 as the "best"
+    best = ops.truncation_selection(pop, size=1)
+    assert pop[0] == best[0]
+
+    # But now let's set that best to a NaN, which *should* force the other
+    # individual to be selected.
+    pop[0].fitness = nan
+
+    best = ops.truncation_selection(pop, size=1)
+    assert pop[1] == best[0]
