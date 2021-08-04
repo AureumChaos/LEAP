fcc6580b3797755b5506bf9a4055a2d84fd617a6
Mark Coletti <colettima@ornl.gov>
2021-08-03 20:59:28 -0400

    Minor code reformatting.

--- examples/simple/simple.py
+++ examples/simple/simple.py
@@ -8,7 +8,6 @@ import os
 from leap_ec import test_env_var
 from leap_ec.simple import ea_solve
 
-
 # When running the test harness, just run for two generations
 # (we use this to quickly ensure our examples don't get bitrot)
 if os.environ.get(test_env_var, False) == 'True':
@@ -24,12 +23,13 @@ def function(values):
     """A simple fitness function, evaluating the sum of squared parameters."""
     return sum([x ** 2 for x in values])
 
+
 ##############################
 # Entry point
 ##############################
 if __name__ == '__main__':
     ea_solve(function,
-            generations=generations,
-            bounds=[(-5.12, 5.12) for _ in range(5)],
-            viz=True,
-            mutation_std=0.1)
+             generations=generations,
+             bounds=[(-5.12, 5.12) for _ in range(5)],
+             viz=True,
+             mutation_std=0.1)
