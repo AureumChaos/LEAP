b38042225ea0aed8f4989aa162c8631f52b1ceec
Mark Coletti <colettima@ornl.gov>
2022-05-13 15:44:08 -0400

    Added stubs for three new operators

--- leap_ec/multiobjective/ops.py
+++ leap_ec/multiobjective/ops.py
@@ -2,3 +2,25 @@
 """
     LEAP pipeline operators for multiobjective optimization.
 """
+from typing import Iterator
+import random
+from toolz import curry
+
+import numpy as np
+
+from leap_ec.ops import compute_expected_probability, listlist_op, iteriter_op
+
+@curry
+@listlist_op
+def fast_nondominated_sort(population: list) -> list:
+    raise NotImplementedError
+
+@curry
+@listlist_op
+def crowding_distance_calc(population: list) -> list:
+    raise NotImplementedError
+
+@curry
+@listlist_op
+def sort_by_dominance(population: list) -> list:
+    raise NotImplementedError
