626313f633b90bdfebe00dd71c879c3bfacb4ba9
Mark Coletti <colettima@ornl.gov>
2022-05-13 15:51:52 -0400

    Added comments for fast_nondominated_sort and parents optional parameter

--- leap_ec/multiobjective/ops.py
+++ leap_ec/multiobjective/ops.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 """
     LEAP pipeline operators for multiobjective optimization.
+
+    For now this just implements NSGA-II, but other multiobjective approaches
+    will eventually be included.
+
+    TODO Should we prepend 'nsgaii' in anticipation of adding other functions
+    for other MO methods?
 """
 from typing import Iterator
 import random
@@ -10,16 +16,32 @@ import numpy as np
 
 from leap_ec.ops import compute_expected_probability, listlist_op, iteriter_op
 
+##############################
+# fast_nondominated_sort operator
+##############################
 @curry
 @listlist_op
-def fast_nondominated_sort(population: list) -> list:
+def fast_nondominated_sort(population: list, parents: list = None) -> list:
+    """ This does the NSGA-II
+    :param population: population to be ranked
+    :param parents: optional parents population to be included with the ranking
+        process
+    """
     raise NotImplementedError
 
+
+##############################
+# crowding_distance_calc operator
+##############################
 @curry
 @listlist_op
 def crowding_distance_calc(population: list) -> list:
     raise NotImplementedError
 
+
+##############################
+# sort_by_dominance operator
+##############################
 @curry
 @listlist_op
 def sort_by_dominance(population: list) -> list:
