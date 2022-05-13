043c3b41b799088cbd2ff534750857bb9052d914
Mark Coletti <colettima@ornl.gov>
2022-05-13 17:38:41 -0400

    First cut of fast_nondominated_sort. Added stub for specialized tournament_selection.

--- leap_ec/multiobjective/README.md
+++ leap_ec/multiobjective/README.md
@@ -1,1 +1,16 @@
 This sub-package is for LEAP support for multiobjective optimization.
+
+As a side-effect some operators will add state to individuals as they pass 
+through a LEAP pipeline.
+
+`fast_nondominated_sort()` adds these attributes:
+
+* `dominates`, a list
+* `dominated_by`, integer, count of others that dominate current individual
+* `rank`, integer, valid values in regular expression `[1-9]+`
+
+Alternatively, we could have used a sub-class to manage this additional 
+state, but we felt that could unnecessarily complicate implementations, 
+particularly in situations where a user had already defined their own 
+`Individual` subclass for their own specialized needs.  This approach 
+provides more flexability in thtat 
--- leap_ec/multiobjective/ops.py
+++ leap_ec/multiobjective/ops.py
@@ -6,28 +6,84 @@
     will eventually be included.
 
     TODO Should we prepend 'nsgaii' in anticipation of adding other functions
-    for other MO methods?
+    for other MO methods?  Or have an 'nsgaii' sub-sub-package? We have to be
+    mindful of backward compatability.
 """
 from typing import Iterator
 import random
+from itertools import chain.from_iterable
 from toolz import curry
 
 import numpy as np
 
 from leap_ec.ops import compute_expected_probability, listlist_op, iteriter_op
 
+def tournament_selection():
+    """ Tournament selection that takes into consideration rank and crowding
+        distance.
+
+        This should be used instead of leap_ec.ops.tournament_selection for
+        NSGA-II.
+
+    TODO eventually this will be replaced by using a special key function for
+    leap_ec.ops.tournament_selection.
+    """
+    raise NotImplementedError
+
+
 ##############################
 # fast_nondominated_sort operator
 ##############################
 @curry
 @listlist_op
-def fast_nondominated_sort(population: list, parents: list = None) -> list:
-    """ This does the NSGA-II
+def fast_nondominated_sort(population: list, parents: list = []) -> list:
+    """ This implements the NSGA-II fast-non-dominated-sort()
+
     :param population: population to be ranked
     :param parents: optional parents population to be included with the ranking
         process
     """
-    raise NotImplementedError
+    ranks = {1 : []} # rank 1 initially empty
+
+    # First, find rank 1
+    for individual in chain.from_iterable(population, parents):
+        individual.dominates = []
+        individual.dominated_by = 0
+        # there is no rank 0, we're just including in initialization as a
+        # reality check; i.e., if we see rank zeroes floating around while
+        # debugging, we know something went wrong
+        individual.rank = 0
+
+        for other_individual in chain.from_iterable(population, parents):
+            if individual is other_individual:
+                continue
+            if individual > other_individual:
+                individual.dominates.append(other_individual)
+            elif other_individual > individual:
+                individual.dominated_by += 1
+
+        if individual.dominated_by == 0:
+            individual.rank = 1
+            ranks[1].append(individual)
+
+    # Now fill out the remaining ranks
+    i = 1
+    while ranks.get(i, []) != []:
+        next_front = []
+        for individual in ranks[i]:
+            for other_individual in individual.dominates:
+                other_individual.dominated_by -= 1
+                if other_individual.dominated_by == 0:
+                    other_individual.rank = i + 1
+                    next_front.append(other_individual)
+
+        i += 1
+        ranks[i] = next_front
+
+    # the parents will have been updated, too, but the next pipeline operator
+    # will also look at them
+    return population
+
 
 
 ##############################
