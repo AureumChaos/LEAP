808761ad09fe56a0fd52f5b5ff5b882ce8e30c53
Mark Coletti <colettima@ornl.gov>
2022-08-24 13:19:33 -0400

    Pass in expected_num_mutations and probability into the closed function to resolve problems with Gremlin.

--- leap_ec/int_rep/ops.py
+++ leap_ec/int_rep/ops.py
@@ -251,14 +251,19 @@ def genome_mutate_binomial(std,
     assert((probability is None) or (probability >= 0))
     assert((probability is None) or (probability <= 1))
 
-
+    # Is the only reason we're making this a closure is to save from having to
+    # do this calculation with each mutation? -- Mark
     if isinstance(std, Iterable):
         p = np.array([_binomial_p_from_std(n, s) for s in std])
     else:
         p = _binomial_p_from_std(n, std)
 
-    def mutator(genome):
+    def mutator(genome,
+                expected_num_mutations: float = None,
+                probability: float = None):
         """Function to return as a closure."""
+        # Make this check here, too, since this is called within the pipeline
+        # and may be invoked dynamically with different parameters.
         if not isinstance(genome, np.ndarray):
             raise ValueError(("Expected genome to be a numpy array. "
                             f"Got {type(genome)}."))
@@ -277,7 +282,7 @@ def genome_mutate_binomial(std,
         selected_p_values = p if not isinstance(p, Iterable) else p[indices_to_mutate]
         binom_mean = n*selected_p_values  # this will do elementwise multiplication if p is a vector
 
-        # Apply binomial pertebations
+        # Apply binomial perturbations
         additive = np.random.binomial(n, selected_p_values, size=len(indices_to_mutate)) - np.floor(binom_mean)
         mutated = genome[indices_to_mutate] + additive
         genome[indices_to_mutate] = mutated
