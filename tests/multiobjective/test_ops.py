19e4892c1b8bb8655b19342be13173d6da0ef90c
Mark Coletti <colettima@ornl.gov>
2022-09-16 17:56:13 -0400

    Start of test for fast nodominated sort.

--- tests/multiobjective/test_ops.py
+++ tests/multiobjective/test_ops.py
@@ -5,6 +5,7 @@
 import itertools
 import numpy as np
 from leap_ec import Individual
+from leap_ec.multiobjective.problems import SCHProblem
 
 
 def test_sort_by_2nd_objective():
@@ -18,3 +19,19 @@ def test_sort_by_2nd_objective():
     target = [np.array([2.0, 1.0]), np.array([1.0, 1.0]), np.array([2.0, 2.0])]
     for i, ind in enumerate(new_pop):
         assert all(ind.fitness == target[i])
+
+
+def test_fast_nondominated_sort():
+    """ Test for non-dominated sorting """
+    # First set up individuals with two genes of combinations of
+    # [-2,-1,0,1,2]
+    genomes = [np.array(a) for a in itertools.product(range(3),repeat=2)]
+    # we start from the second element to skip (0,0) so it isn't duplicated
+    # to get all the negatives
+    genomes += [a * -1 for a in genomes[1:]]
+
+    # We use the Schaffer's problem for Deb et al since that's the simplest
+    # benchmark.
+    pop = [Individual(genome=g, problem=SCHProblem()) for g in genomes]
+
+    pass
