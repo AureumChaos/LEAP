be77ac0b703ac9d614c299746637b1a432039156
Mark Coletti <colettima@ornl.gov>
2022-10-19 11:47:16 -0400

    First candidate for NSGA-II

--- leap_ec/multiobjective/nsga2.py
+++ leap_ec/multiobjective/nsga2.py
@@ -21,7 +21,16 @@ def nsga_2(max_generations: int, pop_size: int, problem, representation,
            init_evaluate=Individual.evaluate_population,
            start_generation: int = 0,
            context=context):
-    """
+    """ NSGA-II multi-objective evolutionary algorithm
+
+        - Deb, Kalyanmoy, Amrit Pratap, Sameer Agarwal, and T. A. M. T. Meyarivan.
+          "A Fast and Elitist Multiobjective Genetic Algorithm: NSGA-II." IEEE
+          transactions on evolutionary computation 6, no. 2 (2002): 182-197.
+
+    :note: You will need a selection as first operator in `pipeline`, which we
+    recommend to be ops.tournament_selection. This will use Deb's multiobjective
+    criteria for comparing individuals as dictated in `MultiobjectiveProblem`.
+
     :param int max_generations: The max number of generations to run the algorithm for.
         Can pass in float('Inf') to run forever or until the `stop` condition is reached.
     :param int pop_size: Size of the initial population
@@ -59,25 +68,20 @@ def nsga_2(max_generations: int, pop_size: int, problem, representation,
     # Evaluate initial population
     parents = init_evaluate(parents)
 
-    # First do a non-dominated sort of the parents
-    parents = fast_nondominated_sort(parents)
-
     while (generation_counter.generation() < max_generations) and \
             not stop(parents):
         # Execute the operators to create a new offspring population
         offspring = pipe(parents,
+                         *pipeline, # user defined selection, mutation & maybe crossover
+                         fast_nondominated_sort,
                          crowding_distance_calc,
-                         sort_by_dominance,
-                         ops.elitist_survival(parents=parents,
-                                              k=k_elites))
-
-        # if max(offspring) > bsf:  # Update the best-so-far individual
-        #     bsf = max(offspring)
+                         # sort_by_dominance, truncation_selection w/ key should do this implicitly
+                         ops.truncation_selection(parents=parents,
+                                                  size=len(parents),
+                                                  key=lambda x: (x.rank, -x.distance))
 
         parents = offspring  # Replace other_population with offspring
-        generation_counter()  # Increment to the next generation
 
-        # Output the best-so-far individual for each generation
-        # yield (generation_counter.generation(), bsf)
+        generation_counter()  # Increment to the next generation
 
     return parents
