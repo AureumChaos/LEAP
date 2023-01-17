d3d411c3788dba56225102ed0fd9734487ac315a
Mark Coletti <colettima@ornl.gov>
2023-01-17 11:51:05 -0500

    Fixed crowding sort test

--- tests/multiobjective/test_ops.py
+++ tests/multiobjective/test_ops.py
@@ -37,9 +37,9 @@ def generate_test_pop():
     problem = SCHProblem()
     pop = [Individual(genome=np.array(g), problem=problem) for g in range(-2,3)]
     pop.append(pop[-1].clone())
-    
+
     pop = Individual.evaluate_population(pop)
-    
+
     ranks = [
         3, # [4, 16] Note: Shared single-objective fitness with a higher rank
         2, # [1, 9]
@@ -57,7 +57,7 @@ def test_fast_nondominated_sort():
     pop, ranks = generate_test_pop()
 
     sorted_pop = fast_nondominated_sort(pop)
-    
+
     np.testing.assert_array_equal(
         [ind.rank for ind in pop],
         ranks
@@ -68,7 +68,7 @@ def test_rank_ordinal_sort():
     pop, ranks = generate_test_pop()
 
     sorted_pop = rank_ordinal_sort(pop)
-    
+
     np.testing.assert_array_equal(
         [ind.rank for ind in pop],
         ranks
@@ -78,7 +78,9 @@ def test_crowding_distance_calc():
     """ Test of crowding distance calculation """
     pop, _ = generate_test_pop()
 
-    sorted_pop = crowding_distance_calc(pop)
+    sorted_pop = fast_nondominated_sort(pop)
+
+    sorted_pop = crowding_distance_calc(sorted_pop)
 
     # TODO add manual checks to verify correct calculation for crowding
     # distance.
