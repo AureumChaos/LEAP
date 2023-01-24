597cade6209ef82af6b147432d9db283bcdb1ef7
Mark Coletti <colettima@ornl.gov>
2023-01-24 09:38:15 -0500

    Trying a Dask Actor to manage the global birth counter

--- tests/distributed/test_budget.py
+++ tests/distributed/test_budget.py
@@ -2,7 +2,8 @@
     Tests for leap_ec.distrib.async for handling birth budgets
 """
 import numpy as np
-from distributed import Client
+from distributed import Client, Variable
+from math import nan
 
 import leap_ec.ops as ops
 from typing import Iterator
@@ -15,6 +16,7 @@ from leap_ec.individual import Individual
 from leap_ec.binary_rep.initializers import create_binary_sequence
 from leap_ec.binary_rep.ops import mutate_bitflip
 from leap_ec.representation import Representation
+from leap_ec.problem import ScalarProblem
 
 def accumulate():
     """ This pipeline operator accumulates individuals as they move
@@ -37,7 +39,8 @@ def accumulate():
     return _accumulate
 
 
-representation = Representation(create_binary_sequence(3))
+representation = Representation(create_binary_sequence(3),
+                                individual_cls=DistributedIndividual)
 
 def test_meet_budget():
     """ This test is to ensure that we meet our birth budget exactly """
@@ -56,6 +59,7 @@ def test_meet_budget():
                            offspring_pipeline=[
                                ops.random_selection,
                                ops.clone,
+                               ops.evaluate,
                                ops.pool(size=1)]
                            )
 
@@ -64,9 +68,72 @@ def test_meet_budget():
     assert len(inds) == 4
 
 
+class Counter:
+    """ From https://distributed.dask.org/en/stable/actors.html
+
+        Used to manage birth counts in a concurrent safe setting.
+     """
+    n = 0
+
+    def __init__(self):
+        self.n = 0
+
+    def increment(self):
+        self.n += 1
+        return self.n
+
+    def add(self, x):
+        self.n += x
+        return self.n
+
+
+class BrokenProblem(ScalarProblem):
+    """ Intentionally throw an exception at an interval
+
+        The intent is to deterministically create non-viable
+        individuals caused by throwing an exception during
+        evaluations at a predetermined interval.
+    """
+    def __init__(self, n, counter):
+        """ :param n: throw an exception at the `n`th eval """
+        super().__init__(True)
+        self.n = n
+        self.counter = counter
+    def evaluate(self, phenome, *args, **kwargs):
+        self.counter.increment()
+        if self.counter.n == self.n:
+            raise RuntimeError('Dummy Exception')
+        return self.counter.n
+
+
 def test_meet_budget_count_nonviable():
     """ Birth budget counting non-viable individuals """
-    pass
+    with Client() as client:
+        # Create a Counter on a worker
+        future = client.submit(Counter, actor=True)
+        counter = future.result()  # Get back a pointer to that object
+
+        # We accumulate all the evaluated individuals the number of
+        # which should be equal to our birth budget of four.
+        my_accumulate = accumulate()
+
+        pop = steady_state(client=client,
+                           births=4,
+                           init_pop_size=2,
+                           pop_size=2,
+                           representation=representation,
+                           problem=BrokenProblem(3, counter),
+                           evaluated_probe=my_accumulate,
+                           offspring_pipeline=[
+                               ops.random_selection,
+                               ops.clone,
+                               ops.evaluate,
+                               ops.pool(size=1)]
+                           )
+
+    inds = my_accumulate.individuals()
+
+    assert len(inds) == 5
 
 def test_meet_budget_do_not_count_nonviable():
     """ Birth budget without counting non-viable individuals """
